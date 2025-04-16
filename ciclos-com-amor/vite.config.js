73404b69830b0c04bddd1389c2b87913871b80b0
Victor Hugo <173806263+victorprogramador@users.noreply.github.com>
2025-04-16 23:15:24 +0000

    fix extension vite

--- ciclos-com-amor/vite.config.js
+++ ciclos-com-amor/vite.config.js
@@ -1,8 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/Eu-e-Kah/ciclos-com-amor/',
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
 });
