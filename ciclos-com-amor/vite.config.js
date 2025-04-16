98f134e546e2e67cfa35e17fa2302fa8686f9e3d
Victor Hugo <173806263+victorprogramador@users.noreply.github.com>
2025-04-16 17:48:12 +0000

    fix vite

--- ciclos-com-amor/vite.config.js
+++ ciclos-com-amor/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/ciclos-com-amor/", // nome do repositório
-  plugins: [react()]
+  base: '/Eu-e-Kah/ciclos-com-amor/', // Caminho correto para o GitHub Pages
+  plugins: [react()],
 });
