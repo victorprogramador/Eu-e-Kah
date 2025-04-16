c76b7ba0b28cc41aae5d75755d134add2de85ca2
Victor Hugo <173806263+victorprogramador@users.noreply.github.com>
2025-04-16 23:49:40 +0000

    fix grock 3

--- ciclos-com-amor/public/service-worker.js
+++ ciclos-com-amor/public/service-worker.js
@@ -1,13 +1,13 @@
 const CACHE_NAME = "ciclos-com-amor-v1";
 const FILES_TO_CACHE = [
-  "/", // Página inicial
-  "/index.html",
-  "/manifest.json",
-  "/public/icon-192.png",
-  "/public/icon-512.png",
-  "/src/main.jsx",
-  "/src/App.jsx",
-  "/src/index.css",
+  "/Eu-e-Kah/ciclos-com-amor/",
+  "/Eu-e-Kah/ciclos-com-amor/index.html",
+  "/Eu-e-Kah/ciclos-com-amor/manifest.json",
+  "/Eu-e-Kah/ciclos-com-amor/public/icon-192.png",
+  "/Eu-e-Kah/ciclos-com-amor/public/icon-512.png",
+  "/Eu-e-Kah/ciclos-com-amor/src/main.jsx",
+  "/Eu-e-Kah/ciclos-com-amor/src/App.jsx",
+  "/Eu-e-Kah/ciclos-com-amor/src/index.css"
 ];
 
 // Instala o service worker e faz o cache dos arquivos
@@ -24,7 +24,7 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keyList) => {
-      return-horizontal Promise.all(
+      return Promise.all(
         keyList.map((key) => {
           if (key !== CACHE_NAME) {
             return caches.delete(key);
