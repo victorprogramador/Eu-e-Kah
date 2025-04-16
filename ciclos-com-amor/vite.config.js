6d4eceabae533a3d681f470c3c6998525c8c5ba0
Victor Hugo <173806263+victorprogramador@users.noreply.github.com>
2025-04-16 18:17:05 +0000

    test

--- ciclos-com-amor/vite.config.js
+++ ciclos-com-amor/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Eu-e-Kah/ciclos-com-amor/', // Caminho correto para o GitHub Pages
+  base: '/Eu-e-Kah/ciclos-com-amor/',
   plugins: [react()],
 });
